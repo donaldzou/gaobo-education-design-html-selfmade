7648b69a96663a8ea32677482b6d9cbdc45e7ad7
donaldzou <donaldzou@live.hk>
2018-05-11 11:59:43 -0300

    update

--- js/database.js
+++ js/database.js
@@ -1,14 +1,28 @@
-var teacher =[
-    {subject: "DP_Psychology-SL", name: "Maxy Ho老师", slogan: "我行！你不行", course:[{title:"TEST"},{title:"TEST2"}]},
-    {subject: "DP_Psychology-SL", name: "Mia老师", slogan: "我行！你不行"},
-    {subject: "DP_Psychology-SL", name: "许丹阳老师", slogan: "我行！你不行"},
-    {subject: "DP_Psychology-SL", name: "许许阳老师", slogan: "我行！你不行"},
-    {subject: "DP_Psychology-SL", name: "许丹丹老师", slogan: "我行！你不行"},
-    {subject: "DP_Spanish-SL", name: "许阳阳老师", slogan: "我行！你不行"},
-    {subject: "DP_Spanish-SL", name: "许许许老师", slogan: "我行！你不行"},
-    {subject: "DP_Spanish-SL", name: "许丹阳老师", slogan: "我行！你不行"},
-    {subject: "DP_Chinese A-HL", name: "许丹阳老师", slogan: "我行！你不行"},
-    {subject: "MYP_Biology", name: "Danny Xu老师", slogan: "我行！你不行"},
+var teacher = [
+    {
+        subject: "DP_Psychology-SL",
+        name: "Maxy Ho老师",
+        slogan: "我行！你不行",
+        course: [
+            {
+                title: "生产可能性曲线六大考点总结",time:"41:09",
+                time:"41:09",
+            },
+            {
+                title: "生产可能性曲线六大考点总结",
+                time:"41:0933"
+            }
+        ]
+    },
+    { subject: "DP_Psychology-SL", name: "Mia老师", slogan: "我行！你不行", course: [{ title: "生产可能性曲线六大考点总结",time:"41:09",time:"41:09" }, { title: "生产可能性曲线六大考点总结",time:"41:09",time:"41:09" }] },
+    { subject: "DP_Psychology-SL", name: "许丹阳老师", slogan: "我行！你不行", course: [{ title: "生产可能性曲线六大考点总结",time:"41:09",time:"41:09" }, { title: "生产可能性曲线六大考点总结",time:"41:09",time:"41:09" }] },
+    { subject: "DP_Psychology-SL", name: "许许阳老师", slogan: "我行！你不行", course: [{ title: "生产可能性曲线六大考点总结",time:"41:09" }, { title: "生产可能性曲线六大考点总结",time:"41:09" }] },
+    { subject: "DP_Psychology-SL", name: "许丹丹老师", slogan: "我行！你不行", course: [{ title: "生产可能性曲线六大考点总结",time:"41:09" }, { title: "生产可能性曲线六大考点总结",time:"41:09" }] },
+    { subject: "DP_Spanish-SL", name: "许阳阳老师", slogan: "我行！你不行", course: [{ title: "生产可能性曲线六大考点总结",time:"41:09" }, { title: "生产可能性曲线六大考点总结",time:"41:09" }] },
+    { subject: "DP_Spanish-SL", name: "许许许老师", slogan: "我行！你不行", course: [{ title: "生产可能性曲线六大考点总结",time:"41:09" }, { title: "生产可能性曲线六大考点总结",time:"41:09" }] },
+    { subject: "DP_Spanish-SL", name: "许丹阳老师", slogan: "我行！你不行", course: [{ title: "生产可能性曲线六大考点总结",time:"41:09" }, { title: "生产可能性曲线六大考点总结",time:"41:09" }] },
+    { subject: "DP_Chinese A-HL", name: "许丹阳老师", slogan: "我行！你不行", course: [{ title: "生产可能性曲线六大考点总结",time:"41:09" }, { title: "生产可能性曲线六大考点总结",time:"41:09" }] },
+    { subject: "MYP_Biology", name: "Danny Xu老师", slogan: "我行！你不行", course: [{ title: "生产可能性曲线六大考点总结",time:"41:09" }, { title: "生产可能性曲线六大考点总结",time:"41:09" }] },
 ];
 
 
@@ -22,8 +36,8 @@ var options = {
     maxPatternLength: 32,
     minMatchCharLength: 1,
     keys: [
-      "subject", "name"
-  ]
-  };
-  var fuse = new Fuse(teacher, options); // "list" is the item array
+        "subject", "name"
+    ]
+};
+var fuse = new Fuse(teacher, options); // "list" is the item array
 //   var result = fuse.search("DP");
--- js/index.js
+++ js/index.js
@@ -5,6 +5,7 @@ var main_1_nav_offset = main_1_nav.offset().top;
 var menu = $(".navbar.navbar-expand-lg.navbar-light.navbar-right.fixed-top");
 var height = main_1_nav.height() + menu.height() + 30;
 
+
 function menucollapse() {
 
 
@@ -40,7 +41,8 @@ $(document).ready(function () {
     $(window).scroll(menucollapse);
 
     $("body").attr("data-offset", height);
-    console.log(height);
+
+    // console.log(height);
 });
 
 $(".custom_selection a").click(function () {
@@ -51,15 +53,18 @@ $(".custom_selection a").click(function () {
 
 //选择项目显示课程
 
-var type_num, current_html, new_html, program, current_list; //Declare
+var type_num, current_html, new_html, program, current_list, teacher_list, trial_list, trial_name, trial_list_name, height_info_a; //Declare
 
 $("#program_select").change(function () {
+    $(".comfirm_course").hide();
     $('html, body').animate({
         scrollTop: $(".class_selection").offset().top - height - 20
       }, 800);
     $(".class_list").html("");//Clear Course list for every changes
     $(".teacher_list_main").html("");//Clear teacher list for every changes 
-    $(".teacher_selection .text-muted").fadeIn();
+    $(".trial_list_main").html("");
+    $(".teacher_selection .text-muted").show();
+    $(".trial_selection .text-muted").show();
     $(".class_selection .text-muted").addClass("display_none");
     program = $(this).val();
     if(program == "0"){
@@ -69,7 +74,7 @@ $("#program_select").change(function () {
     else{
         $(".class_selection h2").text("2. 请选择您学习的" + program + "科目");   
         current_list = JSON.parse($("."+program+"").text());
-        console.log(current_list);
+        // console.log(current_list);
     
         for (type_num = 0; type_num<current_list.length; type_num++){
             current_html = $(".class_list").html();
@@ -80,28 +85,31 @@ $("#program_select").change(function () {
 
 
         $(".list-group-item").click(function(){//Show Teacher List using database
+            $(".comfirm_course").hide();
+            $(".trial_list_main").html("");
+            $(".trial_selection .text-muted").show();
             $(".teacher_list_main").html(""); 
             $(".teacher_selection .text-muted").hide();
             $(".class_selection .active").removeClass("active");
             $(this).addClass("active");
             
             var selected_course = program+"_"+$(this).text();
-            console.log(selected_course);
-            var teacher_list = fuse.search(selected_course);
-            console.log(teacher_list);
+            // console.log(selected_course);
+            teacher_list = fuse.search(selected_course);
+            // console.log(teacher_list);
             var next_row = 0;
 
             for (var teacher_l = 0; teacher_l<teacher_list.length; teacher_l++){
                 next_row++;
                 if (next_row > 3){
                     current_html = $(".teacher_list_main").html();
-                    new_html = current_html + ' <div class="w-100"></div><div class="col-sm"><div class="card teacher-card material_shadow wow fadeInUp" data-wow-duration="0.5s"><div class="card-body"><img class="card-img-top" src="pic/man1.png" alt="Card image cap"><h5 class="card-title teacher-name">'+teacher_list[teacher_l].name+'</h5><p class="card-text">'+teacher_list[teacher_l].slogan+'</p></div></div>';
+                    new_html = current_html + ' <div class="w-100"></div><div class="col-sm"><div class="card teacher-card material_shadow wow fadeInUp" data-wow-duration="0.5s" value="'+teacher_l+'"><div class="card-body"><img class="card-img-top" src="pic/man1.png" alt="Card image cap"><h5 class="card-title teacher-name">'+teacher_list[teacher_l].name+'</h5><p class="card-text">'+teacher_list[teacher_l].slogan+'</p></div></div>';
                     $(".teacher_list_main").html(new_html);
                     next_row = 1;
                 }
                 else{
                     current_html = $(".teacher_list_main").html();
-                    new_html = current_html + '<div class="col-sm"><div class="card teacher-card material_shadow wow fadeInUp" data-wow-duration="0.5s"><div class="card-body "><img class="card-img-top" src="pic/man1.png" alt="Card image cap"><h5 class="card-title teacher-name">'+teacher_list[teacher_l].name+'</h5><p class="card-text">'+teacher_list[teacher_l].slogan+'</p></div></div>';
+                    new_html = current_html + '<div class="col-sm"><div class="card teacher-card material_shadow wow fadeInUp" data-wow-duration="0.5s" value="'+teacher_l+'"><div class="card-body "><img class="card-img-top" src="pic/man1.png" alt="Card image cap"><h5 class="card-title teacher-name">'+teacher_list[teacher_l].name+'</h5><p class="card-text">'+teacher_list[teacher_l].slogan+'</p></div></div>';
                     $(".teacher_list_main").html(new_html);  
                 }
             }
@@ -118,29 +126,86 @@ $("#program_select").change(function () {
 
 
             $(".teacher-card").click(function(){
+                $(".comfirm_course").hide();
+                $(".trial_list_main").html("");
+                $(".trial_selection .text-muted").hide();
                 $(".teacher-card.active").removeClass("active");
                 $(this).addClass("active");
-                var trial_name = $(".teacher-card .card-title").text();
-                var trial_list = fuse.search(trial_name);
+                trial_name = $(this).attr("value");
+                trial_list = teacher_list[trial_name];
+                // console.log(trial_list);
     
                 next_row=0;
-                for (var n = 0; n < trial_list.length; n++){
-                    var trial_list_name = trial_list[n].course;
-                    for (var m = 0; m<trial_list_name.length; m++){
+                // console.log(next_row);
+                trial_list_name = trial_list.course;
+                for (var n = 0; n < trial_list_name.length; n++){
                         next_row++;
                         if (next_row > 3){
                             current_html = $(".trial_list_main").html();
-                            new_html = current_html + '<div class="w-100"></div><div class="col-sm"><div class="card teacher-card material_shadow wow fadeInUp" data-wow-duration="0.5s"><img class="card-img-top" src="pic/trial.jpg" alt="Card image cap"><div class="card-body "><h5 class="card-title">'+trial_list_name[m].title+'</h5><p class="card-text">HELLOO</p></div>'
+                            new_html = current_html + '<div class="w-100"></div><div class="col-sm"><i class="material-icons trial_info">menu</i><div class="card trial-card material_shadow wow fadeInUp" data-wow-duration="0.5s" value="'+n+'"><div class="info"><p class="info_des"></p></div><img class="card-img-top" src="pic/trial.jpg" alt="Card image cap"><div class="card-body "><h5 class="card-title">'+trial_list_name[n].title+'</h5><p class="card-text">'+trial_list.name+'    |    '+trial_list_name[n].time+'</p></div>'
                             $(".trial_list_main").html(new_html);
                         }
                         else{
                             current_html = $(".trial_list_main").html();
-                            new_html = current_html + '<div class="col-sm"><div class="card teacher-card material_shadow wow fadeInUp" data-wow-duration="0.5s"><img class="card-img-top" src="pic/trial.jpg" alt="Card image cap"><div class="card-body "><h5 class="card-title">'+trial_list_name[m].title+'</h5><p class="card-text">HELLOO</p></div>'
+                            new_html = current_html + '<div class="col-sm"><i class="material-icons trial_info">menu</i><div class="card trial-card material_shadow wow fadeInUp" data-wow-duration="0.5s" value="'+n+'"><div class="info"><p class="info_des"></p></div><img class="card-img-top" src="pic/trial.jpg" alt="Card image cap"><div class="card-body "><h5 class="card-title">'+trial_list_name[n].title+'</h5><p class="card-text">'+trial_list.name+'    |    '+trial_list_name[n].time+'</p></div>'
                             $(".trial_list_main").html(new_html);
                         }
-
-                    }
                 }
+                if ($(window).width() >= 992){
+                    $('html, body').animate({
+                        scrollTop: $(".trial_list_main").offset().top - height - 110
+                      }, 800);
+                }
+                else{
+                    $('html, body').animate({
+                        scrollTop: $(".trial_list_main").offset().top - main_1_nav.height()
+                      }, 800);
+                }
+                $(".info").css("height", "0px");
+
+
+                $(".trial-card").click(function(){
+                    $(".trial-card.active").removeClass("active");
+                    $(this).addClass("active");
+                    $(".comfirm_course").show();
+                    $(".comfirm_course").addClass("animated fadeInUp");
+                    
+                    $(".comfirm_course .lead").text("您选择了"+program+"项目的"+$(".list-group-item.active").text()+"课程，并且选择了"+$(".teacher-card.active .teacher-name").text()+"的"+$(".trial-card.active .card-title").text()+"试听课");
+                });
+
+                $(".trial_info").click(function(){
+
+                    $(".info.opened_trial_info").removeClass("opened_trial_info");
+                    $(".info.opened_trial_info").css("height", "0px");
+                    
+                    var child = $(".info_des");
+                    var sub_child = $(".info");
+                    var open = $(".info.opened_trial_info");
+                    console.log($(this).parent().find(child).text("老师："+modal_title_teacher+"    |    长度："+modal_title_time));
+
+                    
+                    var k = $(this).next().attr("value")
+                    var modal_title = trial_list_name[k].title;
+                    var modal_title_teacher = trial_list.name;
+                    var modal_title_time = trial_list_name[k].time;
+                    $(this).parent().find(child).text("老师："+modal_title_teacher+"    |    长度："+modal_title_time);
+                    var height_info = $(this).parent().find(child).height();
+                    var sub_height = $(this).parent().find(sub_child);
+                    height_info_a = height_info+"px";
+                    
+
+                    if ($(this).parent().find(open).length == 1){
+                        $(".info.opened_trial_info").css("height", "0px");
+                        $(".info.opened_trial_info").removeClass("opened_trial_info");
+                    }
+                    else{
+                        sub_height.css("height", height_info_a);
+                        sub_height.addClass("opened_trial_info");
+                    }
+
+                })
+
+
                 
                 
 
